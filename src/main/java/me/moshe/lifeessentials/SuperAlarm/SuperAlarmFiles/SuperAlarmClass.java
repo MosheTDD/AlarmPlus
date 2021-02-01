624aa8bce51ba6a40eee2d0df41f379d6e9ff9ec
Moshe Khovailo <moshehov241@gmail.com>
2021-02-01 16:17:33 +0200

    Small modifications.

--- src/main/java/me/moshe/lifeessentials/SuperAlarm/SuperAlarmFiles/SuperAlarmClass.java
+++ src/main/java/me/moshe/lifeessentials/SuperAlarm/SuperAlarmFiles/SuperAlarmClass.java
@@ -1,25 +1,41 @@
 package me.moshe.lifeessentials.SuperAlarm.SuperAlarmFiles;
 
+import me.border.utilities.file.AbstractYamlFile;
+
 import java.applet.AudioClip;
 import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
-import java.util.Random;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Scanner;
 
-public class SuperAlarmClass {
+public class SuperAlarmClass extends AbstractYamlFile {
     static AudioClip clip;
 
+    public SuperAlarmClass(String name, File path) {
+        super(name, path);
+    }
 
 
     public static void startSuperAlarm(){
-        audioManager();
-        unlock();
+        setDate(23, 00);
+    }
+
+    public static void setDate(int h, int m){//FIX
+        while(LocalTime.now().getHour() == h){
+
+        }
+
+
     }
 
     public static synchronized void audioManager() {
         try {
-            File file = new File("C:\\Users\\97250\\IdeaProjects\\LifeEssentials\\src\\main\\java\\me\\moshe\\lifeessentials\\SuperAlarm\\SuperAlarmFiles\\TestAudio.wav");//FIX
+            File file = new File("C:\\Users\\97250\\IdeaProjects\\LifeEssentials\\src\\main\\java\\me\\moshe\\lifeessentials\\SuperAlarm\\SuperAlarmFiles\\TestAudio.wav");
             clip = (AudioClip) file.toURI().toURL().getContent();
         } catch (MalformedURLException ex) {
             System.out.println(ex);
